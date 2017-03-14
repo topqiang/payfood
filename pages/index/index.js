0e4127a0bcb1b145f48a11cfdfc3190a55532508
topqiang <git>
2017-03-14 22:41:55 +0800

    0314

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,9 +3,52 @@
 var app = getApp()
 Page({
   data: {
-    
+    bolist: [
+      {
+        "id": 1,
+        "boardnum": 2,
+        "mannum": 4,
+        "status": 0
+      },
+      {
+        "id": 1,
+        "boardnum": 2,
+        "mannum": 4,
+        "status": 1
+      },
+      {
+        "id": 1,
+        "boardnum": 2,
+        "mannum": 4,
+        "status": 1
+      },
+      {
+        "id": 1,
+        "boardnum": 2,
+        "mannum": 4,
+        "status": 0
+      }
+    ]
   },
   onLoad: function () {
     
+  },
+  showme: function ( e ) {
+    var status = e.currentTarget.dataset.status;
+    if( status == 0 ){
+      wx.navigateTo({
+        url: '/pages/logs/logs'
+      });
+    }else{
+      wx.showModal({
+        title: '提醒',
+        content: '该餐桌已被预订！',
+        success: function(res) {
+          if (res.confirm) {
+            console.log('用户点击确定')
+          }
+        }
+      });
+    }
   }
 })
--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -5,10 +5,6 @@ Page({
     logs: []
   },
   onLoad: function () {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(function (log) {
-        return util.formatTime(new Date(log))
-      })
-    })
+  
   }
 })
